5bac842a8cfea4f5e901f2224801e7d651abc161
bhakin <103210246+bhakin@users.noreply.github.com>
2022-04-30 01:14:54 -0700

    Update SkillTest.java

--- SkillTest.java
+++ SkillTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillTest {
     @Test
     public void test1(){
-        assertEquals(10, SkillDemo.multiply(5,5));
+        assertEquals(10, SkillDemo.multiply(5,2));
     }
 }
