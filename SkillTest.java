41d5469224b653893aa161d3a433cd95b78c7579
bhakin <103210246+bhakin@users.noreply.github.com>
2022-04-30 01:13:00 -0700

    new

--- SkillTest.java
+++ SkillTest.java
@@ -1,8 +1,7 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 
-public class Test {
-
+public class SkillTest {
     @Test
     public void test1(){
         assertEquals(10, SkillDemo.multiply(5,5));
